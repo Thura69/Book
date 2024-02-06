ed3b37c45d5d10e32f933a2b4952e3cde4f96f16
Thura69 <spyking64@gmail.com>
2024-02-06 22:09:37 +0630

    ads

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    // Add the Bidvertiser script here
+    const script = document.createElement('script');
+    script.src = `//bdv.bidvertiser.com/BidVertiser.dbm?pid=Bidvertiser2091116&bid=1927833`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    // Cleanup the script when the component unmounts
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
